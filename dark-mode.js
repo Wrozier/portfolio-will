a4bf6d81b6e13f3dc2e1dfb515549f1a79ee9bbf
William Rozier <williamrozier@icloud.com>
2023-08-09 14:48:47 -0400

    dark mode

--- dark-mode.js
+++
@@ -1,8 +0,0 @@
-// dark-mode.js
-
-const darkModeToggleBtn = document.getElementById('darkModeToggleBtn');
-const body = document.body;
-
-darkModeToggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
